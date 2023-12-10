26c7921f9b3e846ec33374d34fdc47a897f2810a
CodeAritraDhank <aridhank21205@gmail.com>
2023-12-10 19:46:39 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,30 +1,69 @@
-import React, { Component } from 'react'
-import Navbar from './components/Navbar'
-import { NewsComponent } from './components/NewsComponent'
-import {
-     BrowserRouter,
-     Routes,
-     Route,
-   } from "react-router-dom";
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import { NewsComponent } from "./components/NewsComponent";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Navbar/>
+        <Navbar />
         <Routes>
-        <Route path="/" element={<NewsComponent pageSize="21" country="in" category=""/>}/>
-        <Route path="/Business" element={<NewsComponent pageSize="21" country="in" category="Business"/>}/>
-        <Route path="/Entertainment" element={<NewsComponent pageSize="21" country="in" category="Entertainment"/>}/>
-        <Route path="/Health" element={<NewsComponent pageSize="21" country="in" category="Health"/>}/>
-        <Route path="/Sports" element={<NewsComponent pageSize="21" country="in" category="Sports"/>}/>
-        <Route path="/Science" element={<NewsComponent pageSize="21" country="in" category="Science"/>}/>
-        <Route path="/Technology" element={<NewsComponent pageSize="21" country="in" category="Technology"/>}/>
-        <Route path="/General" element={<NewsComponent pageSize="21" country="in" category="General"/>}/>
+          <Route
+            path="/"
+            element={<NewsComponent pageSize="21" country="in" category="" />}
+          />
+
+          <Route
+            path="/Business"
+            element={
+              <NewsComponent pageSize="21" country="in" category="Business" />
+            }
+          />
+          <Route
+            path="/Entertainment"
+            element={
+              <NewsComponent
+                pageSize="21"
+                country="in"
+                category="Entertainment"
+              />
+            }
+          />
+          <Route
+            path="/Health"
+            element={
+              <NewsComponent pageSize="21" country="in" category="Health" />
+            }
+          />
+          <Route
+            path="/Sports"
+            element={
+              <NewsComponent pageSize="21" country="in" category="Sports" />
+            }
+          />
+          <Route
+            path="/Science"
+            element={
+              <NewsComponent pageSize="21" country="in" category="Science" />
+            }
+          />
+          <Route
+            path="/Technology"
+            element={
+              <NewsComponent pageSize="21" country="in" category="Technology" />
+            }
+          />
+          <Route
+            path="/General"
+            element={
+              <NewsComponent pageSize="21" country="in" category="General" />
+            }
+          />
         </Routes>
       </BrowserRouter>
-    )
+    );
   }
 }
 
-export default App
+export default App;
